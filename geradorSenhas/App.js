d01cf746fe435519d626da0d930553a90cc9bd6d
elisascarano <elisatscarano@gmail.com>
2024-10-23 17:56:31 +0000

    oii

--- geradorSenhas/App.js
+++ geradorSenhas/App.js
@@ -1,87 +1,117 @@
 import { useState } from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity, Modal } from 'react-native';
-import { ModalPassword } from './src/components/modal/index'
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SavedPasswords from './src/screens/SavedPasswords';
+import { ModalPassword } from './src/components/modal/index';
 
 let charset = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-export default function App() {
-  const [senhaGerada, setSenhaGerada] = useState("")
-  const [modalVisible, setModalVisible] = useState(false);
 
-  function gerarSenha(){
-    
-    let senha = "";
+const Stack = createStackNavigator();
 
-    for (let i = 0, n = charset.length; i < 10; i++){
-      senha += charset.charAt(Math.floor(Math.random() * n));
-    }
+function HomeScreen({navigation}) { 
+const [senhaGerada, setSenhaGerada] = useState("");
+const [modalVisible, setModalVisible] = useState(false);
+const [savedPasswords, setSavedPasswords] = useState([]);
 
-    setSenhaGerada(senha);
-    setModalVisible(true);
+function gerarSenha() {
 
+  let senha = "";
+
+  for (let i = 0, n = charset.length; i < 10; i++) {
+    senha += charset.charAt(Math.floor(Math.random() * n));
   }
 
+  setSenhaGerada(senha);
+  setModalVisible(true);
 
-  return (    
-    <View style={styles.container}>
-      <Image
-        source={require("./src/img/logo.png")}
-        style={styles.logo}
+}
 
-
-      />
-
-      <Text style={styles.title}> LockGen </Text>
-
-      <TouchableOpacity style={styles.button} onPress={gerarSenha}>
-        <Text style={styles.textButton}> Gerar Senha </Text>
-      </TouchableOpacity>
-
-      <Modal visible={modalVisible} animationType="fade" transparent={true} >
-      <ModalPassword senha={senhaGerada} handleClose={ () => setModalVisible(false) } />
-      </Modal>
+function salvarSenha() {
+  setSavedPasswords(prevPasswords => {
+    const updatedPasswords = [...prevPasswords, senhaGerada];
+    setModalVisible(false); 
+    navigation.navigate('SavedPasswords', { savedPasswords: updatedPasswords });
+    return updatedPasswords;
+  });
+}
 
 
+return (
+  <View style={styles.container}>
+    <Image
+      source={require("./src/img/logo.png")}
+      style={styles.logo}
 
-      
-      <Text style={styles.senha}> {senhaGerada} </Text>
-    </View>
+
+    />
+
+    <Text style={styles.title}> LockGen </Text>
+
+    <TouchableOpacity style={styles.button} onPress={gerarSenha}>
+      <Text style={styles.textButton}> Gerar Senha </Text>
+    </TouchableOpacity>
+
+    <Modal visible={modalVisible} animationType="fade" transparent={true} >
+      <ModalPassword senha={senhaGerada} handleClose={() => setModalVisible(false)} salvarSenha={salvarSenha} />
+    </Modal>
+
+
+
+
+    <Text style={styles.senha}> {senhaGerada} </Text>
+  </View>
+);
+}
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="SavedPasswords" component={SavedPasswords} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo: {
-    marginBottom: 20,
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 28,
-    marginBottom: 50,
-  },
-  button: {
-    backgroundColor: '#333',
-    width: '70%',
-    height: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 8,
-    padding: 6,    
-  },
-  textButton:{
-    color: '#FFF',
-    fontSize: 15,
-    fontWeight: 'bold',
-  },
-  senha: {
-    marginTop: 20,
-    color: '#333',
-    fontSize: 15,
-    fontWeight: 'bold',
-  },
-});
+      container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      logo: {
+        marginBottom: 20,
+      },
+      title: {
+        fontWeight: 'bold',
+        fontSize: 28,
+        marginBottom: 50,
+      },
+      button: {
+        backgroundColor: '#333',
+        width: '70%',
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 8,
+        padding: 6,
+      },
+      textButton: {
+        color: '#FFF',
+        fontSize: 15,
+        fontWeight: 'bold',
+      },
+      senha: {
+        marginTop: 20,
+        color: '#333',
+        fontSize: 15,
+        fontWeight: 'bold',
+      },
+    });
